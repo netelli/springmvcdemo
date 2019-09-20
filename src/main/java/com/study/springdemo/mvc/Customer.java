ee17285b140bce036838062c2b614e7e9ccde700
netelli <natalli.dev@gmail.com>
2019-09-20 16:24:09 +0300

    Implement max/min validation;

--- src/main/java/com/study/springdemo/mvc/Customer.java
+++ src/main/java/com/study/springdemo/mvc/Customer.java
@@ -1,6 +1,8 @@
 package com.study.springdemo.mvc;
 
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -11,6 +13,18 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName;
+
+    @Min(value = 0, message = "must be >= 0")
+    @Max(value = 10, message = "must be <= 10")
+    private int freePasses;
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
 
     public String getFirstName() {
         return firstName;
